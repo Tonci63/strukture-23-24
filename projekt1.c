d724e50a6e2819c22a76d4e706e16ca8ef175214
Tonci63 <acelic00@fesb.hr>
2023-10-17 13:00:10 +0200

    prvi dio zad

---
+++ projekt1.c
@@ -0,0 +1,56 @@
+﻿/*. Napisati program koji prvo pročita koliko redaka ima datoteka, tj. koliko ima studenata
+zapisanih u datoteci. Nakon toga potrebno je dinamički alocirati prostor za niz struktura
+studenata (ime, prezime, bodovi) i učitati iz datoteke sve zapise. Na ekran ispisati ime,
+prezime, apsolutni i relativni broj bodova.
+Napomena: Svaki redak datoteke sadrži ime i prezime studenta, te broj bodova na kolokviju.
+relatvan_br_bodova = br_bodova/max_br_bodova*100
+*/
+
+
+#define _CRT_SECURE_NO_WARNINGS   //sluzi da ne moramo koristit fopen_s i tako neke gluposti 
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_SIZE (50)
+#define MAX_LINE (1024)
+#define FILE_ERROR_OPEN (-1) 
+
+typedef struct _student {
+	char iname[MAX_SIZE];
+	char surname[MAX_SIZE];
+	double points;
+} Student;
+
+int ReadNORowsInFile() {
+	int counter = 0;
+	FILE* filePointer = NULL;
+	char buffer[MAX_LINE] = { 0 };
+
+	filePointer = fopen("students.txt", "r");
+
+	if (!filePointer) {
+		printf("File is not open!\n");
+		return FILE_ERROR_OPEN;
+	}
+
+	while (!feof(filePointer))
+	{
+		fgets(buffer, MAX_LINE, filePointer);
+		counter++;
+	}
+
+	printf("%d", counter);
+
+	fclose(filePointer);
+
+	return counter;
+}
+
+int main()
+{
+	int noRows = 0;
+	noRows = ReadNORowsInFile();
+
+	printf("%d", noRows);
+
+
+}
