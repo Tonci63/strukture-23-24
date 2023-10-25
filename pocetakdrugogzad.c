9150af93669f9cd6a97b7d6093ea6cfd0e2ff145
Tonci63 <acelic00@fesb.hr>
2023-10-25 19:10:09 +0200

    drugi zad pocetak

---
+++ pocetakdrugogzad.c
@@ -0,0 +1,65 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct _Person;
+typedef struct _Person
+
+Position findLast(Position head) {
+	Position current = head;
+
+	while (current->next != NULL) {
+		current = current->next;
+	}
+	return current; //
+}
+
+int addToEndOfTheList(Position head) {
+
+	Position newPerson = NULL;
+	Position last = NULL;
+
+	newPerson = createPerson();
+
+	if (newPerson) {
+		last = findLast(head);				//nemoj zaminit ove dvi linije koda bi stvoria loop
+		newPerson->next = newPerson;		// u kojem element ima pokazivač na samog sebe
+	}
+
+	return EXIT_SUCCESS;
+
+}
+int printList(Position firstElement) {
+	Position current = firstElement;
+	if (!firstElement)
+	{
+		printf("Empty list!\n");
+	}
+	for (; current != NUll; current = current->next)
+	{
+		printPerson();
+	}
+}
+int main()
+{
+	Person head = {}
+
+
+
+}
+Position createPerson
+
+int menu(Position head) {
+
+	Position person = NULL;
+	while (1) {
+		printf("Enter A(Add to the front of the list), )");
+		scanf°(% c, c);
+	}
+
+
+
+
+}
